7bc703f6c95cb60a5517c46a98b6389537eb8281
bryancandi <quakeaz@gmail.com>
2025-12-15 20:42:14 -0700

    Update URL in C

--- c_url.c
+++ c_url.c
@@ -1,10 +1,21 @@
 /* c_url.c */
 
 #include <stdio.h>
+#include <string.h>
 
 int main(void)
 {
-    https://www.highdesertcomputing.com/
-    printf("hello, world\n");
+    int i = 0;
+    char str[] = "hello, world!";
+
+https://www.kernel.org
+
+    while (i < strlen(str))
+    {
+        putchar(str[i++]);
+        goto https;
+    }
+
+    putchar('\n');
     return 0;
 }
