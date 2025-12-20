99a020866397d63af0d161a95796384467824763
bryancandi <quakeaz@gmail.com>
2025-12-20 09:11:08 -0700

    Initial commit

---
+++ binary.c
@@ -0,0 +1,36 @@
+/*
+ * binary.c 
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2) {
+        printf("Usage: ./binary <binary>\n");
+        return 1;
+    }
+
+    char *end;
+    long value = strtol(argv[1], &end, 2);
+
+    if (*end != '\0') {
+        fprintf(stderr, "Invalid binary number\n");
+        return 1;
+    }
+
+    printf("Decimal: %ld\n", value);
+
+    if (value >= 32 && value <= 126) {
+        printf("ASCII: %c\n", (unsigned char)value);
+    }
+    else if (value >= 0 && value <= 31) {
+        printf("ASCII: non-printable control character\n");
+    }
+    else {
+        printf("ASCII: (out of range)\n");
+    }
+
+    return 0;
+}
