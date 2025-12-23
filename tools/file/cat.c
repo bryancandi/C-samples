ef4e41ba4870c749db07d133910f0a7c4a510b75
Bryan C <7554866+bryancandi@users.noreply.github.com>
2025-12-23 12:58:11 -0700

    Refactor to support multiple file concatenation and improve error handling

--- tools/file/cat.c
+++ tools/file/cat.c
@@ -1,34 +1,51 @@
 /*
  * cat.c
- * Concatenate file to standard output
+ * Concatenate file(s) to standard output
+ * Author: Bryan C.
+ * Date: December 23, 2025
  */
 
 #include <stdio.h>
+#include <stdlib.h>
+
+void filecopy(FILE *, FILE *, char *);
 
 int main(int argc, char *argv[])
 {
-    if (argc < 2 || argc > 2)
-    {
-        printf("Usage: %s FILE\n", argv[0]);
-        return 1;
+    FILE *fp;
+    char *prog = argv[0];  /* program name for stderr */
+
+    if (argc == 1) {  /* no args; copy stdin to stdout */
+        filecopy(stdin, stdout, prog);
     }
-
-    FILE *file = fopen(argv[1], "r");  /* open file in read mode */
-    if (file == NULL)
-    {
-        printf("%s: Cannot access file '%s'\n", argv[0], argv[1]);
-        return 2;
+    else {
+        while (--argc > 0) {
+            if ((fp = fopen(*++argv, "r")) == NULL) {
+                fprintf(stderr, "%s: can't open %s\n", prog, *argv);
+                exit(1);  /* exit(1): read error */
+            }
+            else {
+                filecopy(fp, stdout, prog);
+                fclose(fp);
+            }
+        }
     }
-
-    char c;
-
-    /* Read 'file' one character at a time with fgetc() and print that character until EOF */
-    while ((c = fgetc(file)) != EOF)
-    {
-        printf("%c", c);
+    if (ferror(stdout)) {
+        fprintf(stderr, "%s: error writing stdout\n", prog);
+        exit(2);  /* exit(2): write error */
     }
-
-    fclose(file);
-
     return 0;
+}
+
+/* filecopy: copy file ifp to file ofp */
+void filecopy(FILE *ifp, FILE *ofp, char *prog)
+{
+    int c;
+
+    while ((c = fgetc(ifp)) != EOF) {
+        if (putc(c, ofp) == EOF) {
+            fprintf(stderr, "%s: write error\n", prog);
+            exit(2);
+        }
+    }
 }
