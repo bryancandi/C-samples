16eea465b36d0010019039e2d5de2230c442af34
Bryan C <7554866+bryancandi@users.noreply.github.com>
2025-12-23 13:18:04 -0700

    Refactor for improved error handling and code readability

--- tools/file/cp.c
+++ tools/file/cp.c
@@ -1,50 +1,57 @@
 /*
- * cp.c 
- * Create a copy of a file in binary mode
+ * cp.c
+ * Create a copy of a file in binary mode (cp)
+ * Author: Bryan C.
+ * Date: December 23, 2025
  */
 
 #include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-/* Define BYTE as an 8-bit unsigned integer */
+/* define BYTE as an 8-bit unsigned integer */
 typedef uint8_t BYTE;
 
 int main(int argc, char *argv[])
 {
-    if (argc < 3 || argc > 3)
-    {
-        printf("Usage: %s SOURCE DEST\n", argv[0]);
-        return 1;
+    FILE *src, *dst;
+    char *prog = argv[0];
+
+    if (argc != 3) {
+        fprintf(stderr, "Usage: %s [SRC FILE] [DST FILE]\n", prog);
+        exit(1);
     }
 
-    FILE *src = fopen(argv[1], "rb");  /* open source file and read as binary */
-    if (src == NULL)
-    {
-        printf("%s: Source file '%s' does not exist\n", argv[0], argv[1]);
-        return 2;
+    if ((src = fopen(argv[1], "rb")) == NULL) {  /* open source file and read as binary */
+        fprintf(stderr, "%s: Source file '%s' does not exist\n", prog, argv[1]);
+        exit(2);
     }
 
-    FILE *dst = fopen(argv[2], "wb");  /* open destination file and write as binary */
-    if (dst == NULL)
-    {
-        printf("%s: Cannot create destination file '%s'\n", argv[0], argv[2]);
-        return 3;
+    if ((dst = fopen(argv[2], "wb")) == NULL) {  /* open destination file and write as binary */
+        fprintf(stderr, "%s: Cannot create destination file '%s'\n", prog, argv[2]);
+        fclose(src);  /* src already open, close it */
+        exit(3);
     }
 
     BYTE b;
 
-    /*
-     * Read one byte at a time from source and write it to destination.
-     * Loop continues as long as fread successfully reads a byte, and terminates
-     * when it reaches the end of the file or encounters an error (returns 0).
-     */
-    while (fread(&b, sizeof(b), 1, src) != 0)
-    {
-        fwrite(&b, sizeof(b), 1, dst);
+    while (fread(&b, sizeof(b), 1, src) != 0) {
+        if (fwrite(&b, sizeof(b), 1, dst) != 1) {
+            fprintf(stderr, "%s: write error\n", prog);
+            fclose(src);
+            fclose(dst);
+            exit(4);
+        }
+    }
+    
+    if (ferror(src)) {  /* read error */
+        fprintf(stderr, "%s: read error\n", prog);
+        fclose(src);
+        fclose(dst);
+        exit(5);
     }
 
     fclose(dst);
     fclose(src);
-
     return 0;
 }
