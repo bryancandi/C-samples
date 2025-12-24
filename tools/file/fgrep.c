3a1be63ebbc934a2501c33c39e24a4f2a5eabe9b
bryancandi <quakeaz@gmail.com>
2025-12-23 20:54:01 -0700

    Refactor patterncmp function and comments for improved readability

--- tools/file/fgrep.c
+++ tools/file/fgrep.c
@@ -1,6 +1,5 @@
 /*
  * fgrep.c
- *
  * A simple fixed-string search program, inspired by Unix `fgrep`
  * Matching substrings are highlighted in red using ANSI escape codes
  * To-do: handle files as args
@@ -17,17 +16,15 @@ int main(int argc, char *argv[])
     char *prog = argv[0];
     char buf[BUFSIZ];
 
-    if (argc == 1)
-    {
+    if (argc == 1) {
         fprintf(stderr, "Usage: %s PATTERN [FILE]...\n", prog);
         exit(1);
     }
-    if (argc == 2)
-    {
+    if (argc == 2) {
         patterncmp(argv[1], stdin, prog);
     }
 
-    /* to-do: handle files as args */
+    /* To-do: handle files as args */
 
     return 0;
 }
@@ -38,36 +35,32 @@ void patterncmp(char *pattern, FILE *ifp, char *prog)
     int n = 0;
     char buf[BUFSIZ];
 
-    while ((c = getc(ifp)) != EOF && n < BUFSIZ - 1)
-    {
+    while ((c = getc(ifp)) != EOF && n < BUFSIZ - 1) {
         buf[n++] = c;
 
-        if (c == '\n')
-        {
+        if (c == '\n') {
             buf[n] = '\0';
-            char *p;
+            char *p;          /* pointer for strstr */
             char *pos = buf;  /* updateable pointer to position in buf */
-            int found = 0;    /* line contains at least one match if */
+            int found = 0;    /* default: no match */
 
-            while ((p = strstr(pos, pattern)) != NULL)
-            {
-                int p_start = (p - pos);  /* integer position of pattern (p) in the buffer */
-                int p_end = p_start + strlen(pattern);  /* integer position after pattern in the buffer */
-                for (int i = 0; i < p_start; i++)
-                {
+            while ((p = strstr(pos, pattern)) != NULL) {
+                int p_start = p - pos;  /* integer position of pattern start in pos (buf) */    
+
+                for (int i = 0; i < p_start; i++) {  /* print characters before pattern */
                     putchar(pos[i]);
                 }
-                printf("\033[0;31m");
-                printf("%s", pattern);
-                printf("\033[0m");
-                pos = p + strlen(pattern);
-                found = 1;
+                printf("\033[0;31m%s\033[0m", pattern); /* print pattern in red then reset color */
+
+                found = 1;  /* signal a match */
+                pos = p + strlen(pattern);  /* update pos past the last matched pattern */
             }
-            if (found)
-            {
-                printf("%s", pos);
+
+            if (found) {  /* print remainder of line after last match */
+                fputs(pos, stdout);
             }
-            n = 0;
+
+            n = 0;  /* reset buffer for next line */
         }
     }
 }
