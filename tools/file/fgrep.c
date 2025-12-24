b9b5adf577a3eeac1c074c552397fe51beb42ee2
Bryan C <7554866+bryancandi@users.noreply.github.com>
2025-12-23 19:14:35 -0700

    Rename grep.c to fgrep.c and update header comments for clarity

--- tools/file/fgrep.c
+++ tools/file/fgrep.c
@@ -1,9 +1,6 @@
 /*
- * fgrep.c
- *
- * A simple fixed-string search program, inspired by Unix `fgrep`
- * Matching substrings are highlighted in red using ANSI escape codes
- * To-do: handle files as args
+ * grep.c
+ * to-do: handle files as args
  */
 
 #include <stdio.h>
