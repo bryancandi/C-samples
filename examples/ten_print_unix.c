377b217e4d130209f520d663ef9ea22cab76f4ad
bryancandi <quakeaz@gmail.com>
2025-12-29 00:35:39 -0700

    Initial commit

---
+++ examples/ten_print_unix.c
@@ -0,0 +1,25 @@
+/*
+ * ten_print_unix.c
+ *
+ * "Early C" version of the classic CBM BASIC “10 PRINT” maze program:
+ * 10 PRINT CHR$(205.5+RND(1)); : GOTO 10
+ *
+ * Written for Unix Fourth Edition (c. 1973).
+ * Use Ctrl+\ (Quit signal) to terminate.
+ *
+ * Author: Bryan C.
+ * Date: December 29, 2025
+ */
+
+main()
+{
+    int i;
+    i = 0;
+    while (1) {
+        putchar("/\\"[(rand() >> 8) & 1]);
+        i++;
+        if (i % 80 == 0) {
+            putchar('\n');
+        }
+    }
+}
