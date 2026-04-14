b66ac95becc67bc2c1462e00ca07eefae416146e
Bryan C <7554866+bryancandi@users.noreply.github.com>
2026-04-14 15:39:35 -0700

    Implement uint64_t and simplify output logic

--- examples/lab/fibonacci_1.c
+++ examples/lab/fibonacci_1.c
@@ -2,13 +2,12 @@
  * Print Fibonacci sequence up to Fn (0-93).
  */
 
+#include <stdint.h>
 #include <stdio.h>
 
 int main(void)
 {
-    size_t f1 = 0;
-    size_t f2 = 1;
-    size_t tmp;
+    uint64_t n1 = 0, n2 = 1, tmp;
     int count;
 
     printf("Enter a number (0 to 93): ");
@@ -18,17 +17,13 @@ int main(void)
         return 1;
     }
 
-    printf("0\n");
-
-    while(count > 0)
+    while(count >= 0)
     {
-        tmp = f1;
-        f1 = f1 + f2;
-        f2 = tmp;
-
+        printf("%zu\n", n1);
+        tmp = n1;
+        n1 = n1 + n2;
+        n2 = tmp;
         count--;
-
-        printf("%zu\n", f1);
     }
 
     return 0;
